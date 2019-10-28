45a0b87a5530ea29f0a4c4fa31e81fa7aaf7a055
Keerthanadevi Manivannan <kmanivan@ucsc.edu>
2019-10-28 16:07:40 -0700

    Add settings activity version

--- Android/app/src/main/java/com/project/kitt/SettingsFragment.java
+++ Android/app/src/main/java/com/project/kitt/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.project.kitt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,22 +17,41 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SettingsFragment extends Fragment {
-    ListView listView;
+public class SettingsFragment extends Fragment{
+    ListView listview;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view =  inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-       String [] settingsItems = {"Notifications", "Themes", "Account Settings"};
-       ListView listview = view.findViewById(R.id.listSettings);
+        String[] settingsItems = {"Notifications", "Themes", "Account Settings"};
+        listview = view.findViewById(R.id.listSettings);
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
                 getActivity(), android.R.layout.simple_list_item_1, settingsItems
         );
 
         listview.setAdapter(listViewAdapter);
 
-       return view;
+        return view;
+
+    }
+
+public void onStart(){
+    super.onStart();
+
+    AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            // Do something in response to the click
+            if (position == 0) {
+            //    Intent myIntent= new Intent(view.getContext(), SettingsNotifications.class);
+                Intent myIntent= new Intent(view.getContext(), SettingsNotifications.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        }
+    };
+     listview.setOnItemClickListener(messageClickedHandler);
+
     }
 
 
---
+++ Android/app/src/main/java/com/project/kitt/SettingsNotifications.java
@@ -0,0 +1,16 @@
+package com.project.kitt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsNotifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_list_notifications);
+    }
+
+
+}
